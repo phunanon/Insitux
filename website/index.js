66979dc843346d4725d886a7ea337a00410ec839
Patrick Bowen <phunanon@users.noreply.github.com>
2021-09-07 12:54:46 +0100

    REPL and website improvements

--- website/index.js
+++ website/index.js
@@ -17,8 +17,21 @@ function synHighlight(code) {
     .split("\n")
     .map(line => (line.startsWith(";") ? `<i>${line}</i>` : line))
     .join("\n");
-  build = build.replaceAll(/([0-9.]+)([\s\)\]])/g, "<num>$1</num>$2");
+  build = build.replaceAll(/([0-9]+)([\s\)\]])/g, "<num>$1</num>$2");
   build = build.replaceAll(/(\"[^\"]+\"|\:[^\n\s\[\]]+)/g, `<str>$1</str>`);
   build = build.replaceAll(/\(([^\n\s\[\]]+)/g, "(<op>$1</op>");
-  return build;
+  let newBuild = "",
+    depth = 0;
+  for (let i = 0; i < build.length; ++i) {
+    if ("([".includes(build[i])) {
+      newBuild += `<p${depth}>${build[i]}</p${depth}>`;
+      ++depth;
+    } else if (")]".includes(build[i])) {
+      --depth;
+      newBuild += `<p${depth}>${build[i]}</p${depth}>`;
+    } else {
+      newBuild += build[i];
+    }
+  }
+  return newBuild;
 }
