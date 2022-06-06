4031da88f5c462842df255ad04061673993b6a85
Patrick Bowen <phunanon@users.noreply.github.com>
2022-06-06 14:40:58 +0100

    Transpilation testing

---
+++ testing.js
@@ -0,0 +1,72 @@
+
+var ops = {
+  dec(x) { return x - 1; },
+  str(...args) {
+    return args.join("");
+  },
+  version() {
+    return "22.5.8";
+  },
+  print(...args) {
+    console.log(args.join(""));
+  },
+  "fast+": (...args) => {
+    return args[0] + args[1];
+  },
+  "fast-": (...args) => {
+    return args[0] - args[1];
+  },
+  "fast<": (...args) => {
+    return args[0] < args[1];
+  }
+}
+
+ops["fib"] = function(...args) {
+  var stack = [];
+  stack.push(args[0]);
+  stack.push(2);
+  stack.push("fast<");
+  var op = stack.pop();
+  var params = stack.splice(stack.length - (1 + 1), 2);
+  stack.push(ops[op](...params));
+  if (stack.pop()) {
+    stack.push(args[0]);
+  }
+  else {
+    stack.push(args[0]);
+    stack.push("dec");
+    var op = stack.pop();
+    var params = stack.splice(stack.length - (1 + 0), 1);
+    stack.push(ops[op](...params));
+    stack.push("fib");
+    var op = stack.pop();
+    var params = stack.splice(stack.length - (1 + 0), 1);
+    stack.push(ops[op](...params));
+    stack.push(args[0]);
+    stack.push(2);
+    stack.push("fast-");
+    var op = stack.pop();
+    var params = stack.splice(stack.length - (1 + 1), 2);
+    stack.push(ops[op](...params));
+    stack.push("fib");
+    var op = stack.pop();
+    var params = stack.splice(stack.length - (1 + 0), 1);
+    stack.push(ops[op](...params));
+    stack.push("fast+");
+    var op = stack.pop();
+    var params = stack.splice(stack.length - (1 + 1), 2);
+    stack.push(ops[op](...params));
+  }
+  return stack.pop();
+}
+ops["entry"] = function(...args) {
+  var stack = [];
+  stack.push(28);
+  stack.push("fib");
+  var op = stack.pop();
+  var params = stack.splice(stack.length - (1 + 0), 1);
+  stack.push(ops[op](...params));
+  return stack.pop();
+}
+
+console.log(ops.entry());
